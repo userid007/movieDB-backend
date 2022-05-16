fb70f6f91b83fcfa71ba180072ad010b7213f968
userid007 <shindeakshay7499@gmail.com>
2022-05-17 02:21:50 +0530

    minnor fix

--- src/controllers/public/index.js
+++ src/controllers/public/index.js
@@ -1,9 +1,8 @@
 const Router = require('express').Router;
-const { checkAuth } = require('../../middlewares');
 const { playlist } = require('./playlist.controller')
 const router = Router();
 
 router.route('/playlist/:movieListId')
-  .get(checkAuth, playlist);
+  .get(playlist);
 
 module.exports = router;
